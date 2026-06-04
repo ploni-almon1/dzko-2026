157a4c7221dd297d98a54ddc37f158991696358f
ploni-almon1 <yqxwcedsa@gmail.com>
2026-06-04 12:03:44 +0200

    Update App.js

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { StatusBar } from 'expo-status-bar';
 
 // 👇👇👇 ZDE JE TVOJE CENTRÁLNÍ BARVA PRO CELOU APLIKACI 👇👇👇
-const DEFAULT_THEME_COLOR = '#8B5CF6'; 
+const DEFAULT_THEME_COLOR = '#3A24DC'; 
 
 // --- GENERÁTOR MAPY ---
 const generateMapHtml = (focusLat, focusLng, focusTitle, themeColor) => `
