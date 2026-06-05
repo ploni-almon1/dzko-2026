0726f38481ff8ac75dd12b68735bd056d22cc8a0
ploni-almon1 <yqxwcedsa@gmail.com>
2026-06-05 21:48:49 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1098,7 +1098,7 @@ const styles = StyleSheet.create({
   },
   homeHeroOverlay: {
     position: 'absolute',
-    bottom: 50,
+    bottom: 100,
     width: '100%',
     alignItems: 'center',
   },
