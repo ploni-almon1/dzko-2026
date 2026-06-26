fdd55455af22b1dc950eec1a13194eb6e9d290fd
ploni-almon1 <yqxwcedsa@gmail.com>
2026-06-26 14:17:03 +0200

    Smazán duplicitní loader

--- App.js
+++ App.js
@@ -44,41 +44,6 @@ const safeImage = (val) => {
   return null;
 };
 
-// 👇 NÁŠ NOVÝ VLASTNÍ LOADER 👇
-const CustomLoader = ({ themeColor }) => {
-  const spinValue = useRef(new Animated.Value(0)).current;
-
-  useEffect(() => {
-    Animated.loop(
-      Animated.timing(spinValue, {
-        toValue: 1,
-        duration: 2000, 
-        easing: Easing.linear,
-        useNativeDriver: Platform.OS !== 'web', 
-      })
-    ).start();
-  }, [spinValue]);
-
-  const spin = spinValue.interpolate({
-    inputRange: [0, 1],
-    outputRange: ['0deg', '360deg']
-  });
-
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#F3F4F6' }}>
-      <Animated.Image
-        source={require('./assets/star.png')}
-        style={{
-          width: 50,
-          height: 50,
-          tintColor: themeColor,
-          transform: [{ rotate: spin }]
-        }}
-        resizeMode="contain"
-      />
-    </View>
-  );
-};
 // 👇 FUNKCE PRO PŘIDÁNÍ DO KALENDÁŘE 👇
 const stahniKalendar = (akce) => {
   const denCislo = akce.den.replace(/[^0-9]/g, ''); 
